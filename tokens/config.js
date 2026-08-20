340cb383b936eed6b77f330c9d6d0022ccd84d72
Jack Manners <mann0242@flinders.edu.au>
2026-08-20 16:58:30 +0930

    Single source of truth for APPS_SCRIPT_URL (config.js)

---
+++ tokens/config.js
@@ -0,0 +1,2 @@
+// Single source of truth for both index.html and admin.html.
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyrvKpSqkrc4j6QtGr7LyhhEMvREm3AQFI6UOilWmUwNYsUBRTMPFKC5v6yJNHj6pBf-Q/exec';
